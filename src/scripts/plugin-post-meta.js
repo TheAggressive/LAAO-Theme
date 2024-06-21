78000da281a1a87eb2178820d5f9415b1e485d5a
Shawn Mosher <10968229+TheAggressive@users.noreply.github.com>
2024-06-21 19:58:34 +0000

    Update: Working on structure

--- src/scripts/plugin-post-meta.js
+++ src/scripts/plugin-post-meta.js
@@ -1,53 +1,103 @@
 /**
  * WordPress dependencies
  */
-import { RichText } from '@wordpress/block-editor';
-import { Panel, PanelBody, PanelRow, TextControl } from '@wordpress/components';
+import { MediaUpload, MediaUploadCheck } from '@wordpress/block-editor';
+import { Button, PanelRow, TextControl } from '@wordpress/components';
 import { useEntityProp } from '@wordpress/core-data';
 import { useSelect } from '@wordpress/data';
 import { PluginDocumentSettingPanel } from '@wordpress/editor';
-import { __ } from '@wordpress/i18n';
 import { registerPlugin } from '@wordpress/plugins';
+
+const AttachmentImage = ({ imageId, size = 'full' }) => {
+
+	const { image } = useSelect((select) => ({
+		image: select('core').getMedia(imageId),
+	}));
+
+	const imageAttributes = () => {
+		let attributes = {
+			src: image.source_url,
+			alt: image.alt_text,
+			className: `attachment-${size} size-${size}`,
+			width: image.media_details.width,
+			height: image.media_details.height,
+		};
+		if (image.media_details && image.media_details.sizes && image.media_details.sizes[size]) {
+			attributes.src = image.media_details.sizes[size].source_url;
+			attributes.width = image.media_details.sizes[size].width;
+			attributes.height = image.media_details.sizes[size].height;
+		}
+
+		return attributes;
+	};
+
+	return (
+		<>
+			{image && (
+				<img {...imageAttributes()} />
+			)}
+		</>
+	)
+}
 
 const MetaBlockField = () => {
 	const postType = useSelect((select) => select('core/editor').getCurrentPostType());
 	// get post id from the core/editor store
 	const postId = useSelect((select) => select('core/editor').getCurrentPostId());
 	const [meta, setMeta] = useEntityProp('postType', postType, 'meta', postId);
+	const { image } = useSelect((select) => ({
+		image: select('core').getMedia(meta['location']),
+	}));
+
+	console.log('image', image);
+	console.log('location', meta['location']);
 
 	return (
 		<>
-			<Panel title="Meta Block">
-				<PanelBody>
-					<TextControl
-						label="Meta Block Field"
-						value={meta['author'] || ''}
-						onChange={(newValue) =>
-							setMeta({
+			<PanelRow>
+				<TextControl
+					label="Meta Block Field"
+					value={meta['author'] || ''}
+					onChange={(newValue) =>
+						setMeta({
+							...meta,
+							['author']: newValue,
+						})
+					}
+				/>
+			</PanelRow>
+			<PanelRow>
+				{image ? (
+					<>
+						<AttachmentImage imageId={meta['location']} size='medium' />
+						<Button onClick={() => setMeta(
+							{
 								...meta,
-								['author']: newValue,
-							})
-						}
-					/>
-				</PanelBody>
-			</Panel>
-			<Panel title="Meta Block 2">
-				<PanelBody>
-					<PanelRow>My Panel Inputs and Labels</PanelRow>
-					<RichText
-						tagName="p"
-						value={meta['picture_id'] || ''}
-						allowedFormats={['core/bold', 'core/italic']}
-						onChange={(newValue) =>
-							setMeta({
+								['location']: '',
+							}
+						)} isSecondary>
+							Remove Image
+						</Button>
+					</>
+				) : (
+					<MediaUploadCheck>
+						<MediaUpload
+							onSelect={(media) => setMeta({
 								...meta,
-								['author']: newValue,
-							})
-						}
-						placeholder={__('Heading...')}
-					/>
-				</PanelBody>
-			</Panel>
+								['location']: media.id.toString(),
+							})}
+							allowedTypes={['image']}
+							value={meta['location'] || null}
+							render={({ open }) => (
+								<Button onClick={open} isPrimary>
+									Upload Image
+								</Button>
+							)}
+						/>
+					</MediaUploadCheck>
+				)
+				}
+			</PanelRow>
 		</>
 	);
 };
@@ -55,7 +105,7 @@ const MetaBlockField = () => {
 registerPlugin('plugin-sidebar-9ee4a6', {
 	render: () => (
 		<PluginDocumentSettingPanel
-			name="editiorial-options"
+			name="eEditorial-options"
 			title="Editorial Options"
 			className="editorial-options">
 			<MetaBlockField />
