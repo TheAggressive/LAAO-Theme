7d3af5e0e1ee1e2ab99852b3338e747d7e8bf54a
Shawn Mosher <10968229+TheAggressive@users.noreply.github.com>
2024-06-22 16:46:52 +0000

    Update: Working on structure

--- src/scripts/plugin-post-meta.js
+++ src/scripts/plugin-post-meta.js
@@ -2,10 +2,11 @@
  * WordPress dependencies
  */
 import { MediaUpload, MediaUploadCheck } from '@wordpress/block-editor';
-import { Button, PanelRow, TextControl } from '@wordpress/components';
+import { Button, PanelRow, SelectControl, TextControl } from '@wordpress/components';
 import { useEntityProp } from '@wordpress/core-data';
 import { useSelect } from '@wordpress/data';
 import { PluginDocumentSettingPanel } from '@wordpress/editor';
+import { __ } from '@wordpress/i18n';
 import { registerPlugin } from '@wordpress/plugins';
 
 const AttachmentImage = ({ imageId, size = 'full' }) => {
@@ -40,7 +41,49 @@ const AttachmentImage = ({ imageId, size = 'full' }) => {
 	)
 }
 
-const MetaBlockField = () => {
+const AuthorField = () => {
+	const postType = useSelect((select) => select('core/editor').getCurrentPostType());
+	const postId = useSelect((select) => select('core/editor').getCurrentPostId());
+	const [meta, setMeta] = useEntityProp('postType', postType, 'meta', postId);
+	return (
+		<PluginDocumentSettingPanel
+			name="author-options"
+			title={__('Author Options', 'laao')}
+			className="author-options">
+			<PanelRow>
+				<SelectControl
+					label="Credits"
+					value={meta['by_options'] || 'Please Select'}
+					options={[
+						{ label: 'Please Select', value: 'Please Select' },
+						{ label: 'By', value: 'By' },
+						{ label: 'Story / Photo By', value: 'Story / Photo By' },
+						{ label: 'Story / Photos By', value: 'Story / Photos By' },
+					]}
+					onChange={(value) => setMeta({
+						...meta,
+						['by_options']: value,
+					})}
+					__nextHasNoMarginBottom
+				/>
+			</PanelRow>
+			<PanelRow>
+				<TextControl
+					value={meta['author'] || ''}
+					label={__('Author', 'laao')}
+					onChange={(value) =>
+						setMeta({
+							...meta,
+							['author']: value,
+						})
+					}
+				/>
+			</PanelRow>
+		</PluginDocumentSettingPanel>
+	);
+};
+
+const ImageFields = () => {
 	const postType = useSelect((select) => select('core/editor').getCurrentPostType());
 	// get post id from the core/editor store
 	const postId = useSelect((select) => select('core/editor').getCurrentPostId());
@@ -49,37 +92,29 @@ const MetaBlockField = () => {
 		image: select('core').getMedia(meta['location']),
 	}));
 
-	console.log('image', image);
-	console.log('location', meta['location']);
-
 	return (
-		<>
-			<PanelRow>
-				<TextControl
-					label="Meta Block Field"
-					value={meta['author'] || ''}
-					onChange={(newValue) =>
-						setMeta({
-							...meta,
-							['author']: newValue,
-						})
-					}
-				/>
-			</PanelRow>
-			<PanelRow>
-				{image ? (
-					<>
-						<AttachmentImage imageId={meta['location']} size='medium' />
-						<Button onClick={() => setMeta(
+		<PluginDocumentSettingPanel
+			name="image-options"
+			title={__('Image Options', 'laao')}
+			className="image-options">
+			{image ? (
+				<>
+					<PanelRow>
+						<AttachmentImage imageId={meta['location']} onClick={{ open }} size='medium' />
+					</PanelRow>
+					<PanelRow>
+						<Button variant='secondary' isDestructive onClick={() => setMeta(
 							{
 								...meta,
 								['location']: '',
 							}
-						)} isSecondary>
-							Remove Image
+						)} >
+							{__('Remove Image', 'laao')}
 						</Button>
-					</>
-				) : (
+					</PanelRow>
+				</>
+			) : (
+				<PanelRow>
 					<MediaUploadCheck>
 						<MediaUpload
 							onSelect={(media) => setMeta({
@@ -89,26 +124,32 @@ const MetaBlockField = () => {
 							allowedTypes={['image']}
 							value={meta['location'] || null}
 							render={({ open }) => (
-								<Button onClick={open} isPrimary>
-									Upload Image
+								<Button variant='primary' onClick={open}>
+									{__('Upload Image', 'laao')}
 								</Button>
 							)}
 						/>
 					</MediaUploadCheck>
-				)
-				}
-			</PanelRow>
-		</>
+				</PanelRow>
+			)
+			}
+		</PluginDocumentSettingPanel>
 	);
+
 };
 
-registerPlugin('plugin-sidebar-9ee4a6', {
+registerPlugin('laao-author-options', {
 	render: () => (
-		<PluginDocumentSettingPanel
-			name="eEditorial-options"
-			title="Editorial Options"
-			className="editorial-options">
-			<MetaBlockField />
-		</PluginDocumentSettingPanel>
+		<>
+			<AuthorField />
+		</>
+	),
+});
+
+registerPlugin('laao-image-options', {
+	render: () => (
+		<>
+			<ImageFields />
+		</>
 	),
 });
