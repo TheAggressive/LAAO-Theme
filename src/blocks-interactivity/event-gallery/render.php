3ce0b726908203b3bf79e134bb9e0f61bfd60e09
Shawn Mosher <10968229+TheAggressive@users.noreply.github.com>
2024-07-19 15:50:18 +0000

    Update: Working on structure

---
+++ src/blocks-interactivity/event-gallery/render.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * PHP file to use when rendering the block type on the server to show on the front end.
+ *
+ * The following variables are exposed to the file:
+ *     $attributes (array): The block attributes.
+ *     $content (string): The block default content.
+ *     $block (WP_Block): The block instance.
+ *
+ * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
+ */
+?>
+
+<pre>
+	<?php
+		print_r( $attributes );
+	?>
+</pre>
+
+<p>this output</p>
---
+++ src/blocks-interactivity/event-gallery/view.js
@@ -0,0 +1,20 @@
+/**
+ * WordPress dependencies
+ */
+import { getContext, store } from "@wordpress/interactivity";
+
+store('event-gallery', {
+	actions: {
+		toggle: () => {
+			const context = getContext();
+			context.isOpen = !context.isOpen;
+		},
+	},
+	callbacks: {
+		logIsOpen: () => {
+			const { isOpen } = getContext();
+			// Log the value of `isOpen` each time it changes.
+			console.log(`Is open: ${isOpen}`);
+		},
+	},
+});
