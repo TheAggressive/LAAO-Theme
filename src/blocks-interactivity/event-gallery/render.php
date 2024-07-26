46bb0b15c1d71e6cd66ff3ba9e6fccd5d7531d4e
Shawn Mosher <10968229+TheAggressive@users.noreply.github.com>
2024-07-26 16:35:15 +0000

    Update: Working on structure

--- src/blocks-interactivity/event-gallery/render.php
+++ src/blocks-interactivity/event-gallery/render.php
@@ -67,7 +67,7 @@ if ( ! function_exists( 'laao_render_event_gallery_lightbox' ) ) {
 						</a>
 					</li>
 				</ul>
-				<a class="aggressive-lightbox-close" href="#" aria-label="Close lightbox">
+				<a class="aggressive-lightbox-close" href="#" aria-label="Close lightbox" data-wp-on--click='actions.handleClick'>
 					<svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
 						<path d="M764.288 214.592 512 466.88 259.712 214.592a31.936 31.936 0 0 0-45.12 45.12L466.752 512 214.528 764.224a31.936 31.936 0 1 0 45.12 45.184L512 557.184l252.288 252.288a31.936 31.936 0 0 0 45.12-45.12L557.12 512.064l252.288-252.352a31.936 31.936 0 1 0-45.12-45.184z"></path>
 					</svg>
