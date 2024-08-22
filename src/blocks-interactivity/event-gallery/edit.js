742d2eda0a4ce8da57e96ea23acc613afe445d4f
Shawn Mosher <10968229+TheAggressive@users.noreply.github.com>
2024-08-22 18:44:04 +0000

    Update: Working on structure

--- src/blocks-interactivity/event-gallery/edit.js
+++ src/blocks-interactivity/event-gallery/edit.js
@@ -2,8 +2,8 @@ import { BlockControls, BlockIcon, MediaPlaceholder, MediaUpload, MediaUploadChe
 import { ToolbarButton, ToolbarGroup } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
-export default function Edit(props) {
-	const hasImages = props.attributes.images.length > 0;
+export default function Edit({ attributes, setAttributes }) {
+	const hasImages = attributes.images.length > 0;
 
 	return (
 		<>
@@ -14,40 +14,38 @@ export default function Edit(props) {
 							multiple
 							gallery
 							addToGallery={true}
-							onSelect={(newImages) => props.setAttributes({ images: newImages })}
+							onSelect={(newImages) => setAttributes({ images: newImages })}
 							allowedTypes={['image']}
-							value={props.attributes.images.map((image) => image.id)}
+							value={attributes.images.map((image) => image.id)}
 							render={({ open }) => <ToolbarButton onClick={open}>{__('Edit Gallery', 'scrollable-gallery')}</ToolbarButton>}
 						/>
 					</MediaUploadCheck>
 				</ToolbarGroup>
 			</BlockControls>
-			<div {...useBlockProps()}>
-				{hasImages && (
-					<div className='wp-block-event-gallery'>
-						{props.attributes.images.map((image, index) => {
-							return (
-								<figure key={image.id} className='wp-block-event-gallery-item'>
-									<img className={`wp-image-${image.id}`} key={index} loading='lazy' data-link={image.link} data-id={image.id} src={image.sizes.thumbnail.url} />
-								</figure>
-							);
-						})}
-					</div>
-				)}
-				{!hasImages && (
-					<MediaPlaceholder
-						multiple
-						gallery
-						allowedTypes={['image']}
-						icon={<BlockIcon icon='format-gallery' />}
-						labels={{
-							title: 'Scrollable Gallery',
-							instructions: 'Create an awesome scrollable gallery.',
-						}}
-						onSelect={(newImages) => props.setAttributes({ images: newImages })}
-					/>
-				)}
-			</div>
+			{hasImages && (
+				<div {...useBlockProps()} className={`wp-block-event-gallery has-event-gallery-images-${attributes.images.length}`}>
+					{attributes.images.map((image, index) => {
+						return (
+							<figure key={image.id} className='wp-block-event-gallery-item'>
+								<img className={`wp-image-${image.id}`} key={index} loading='lazy' data-link={image.link} data-id={image.id} src={image.sizes.full.url} />
+							</figure>
+						);
+					})}
+				</div>
+			)}
+			{!hasImages && (
+				<MediaPlaceholder
+					multiple
+					gallery
+					allowedTypes={['image']}
+					icon={<BlockIcon icon='format-gallery' />}
+					labels={{
+						title: 'Scrollable Gallery',
+						instructions: 'Create an awesome scrollable gallery.',
+					}}
+					onSelect={(newImages) => setAttributes({ images: newImages })}
+				/>
+			)}
 		</>
 	);
 }
