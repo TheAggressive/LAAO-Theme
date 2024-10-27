c4a398d8bb8ecef7737cba4d2bf6d3c2960b061d
Shawn Mosher <10968229+TheAggressive@users.noreply.github.com>
2024-10-27 20:08:30 +0000

    Update: Working on structure

--- src/blocks-interactivity/event-gallery/save.js
+++ src/blocks-interactivity/event-gallery/save.js
@@ -1,11 +1,22 @@
 export default function Save(props) {
 	return (
-		<div className='wp-block-event-gallery'>
+		<div className="wp-block-event-gallery">
 			{props.attributes.images.map((image, index) => {
 				return (
-					<figure key={image.id} className='wp-block-event-gallery-item'>
+					<figure
+						key={image.id}
+						className="wp-block-event-gallery-item"
+					>
 						<a href={image.url}>
-							<img className={`wp-image-${image.id}`} key={index} loading='lazy' data-link={image.link} data-id={image.id} src={image.sizes.thumbnail.url} />
+							<img
+								className={`wp-image-${image.id}`}
+								key={index}
+								loading="lazy"
+								data-link={image.link}
+								data-id={image.id}
+								src={image.sizes.thumbnail.url}
+								alt={image.alt}
+							/>
 						</a>
 					</figure>
 				);
